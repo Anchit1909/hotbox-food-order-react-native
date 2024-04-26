a2675eeef704fb189296c6cb33a798921cc1f4ff
Anchit Sinha <anchit1909@gmail.com>
2024-04-26 12:50:42 +0530

    feat: type safe components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-import { useColorScheme } from "@/src/components/useColorScheme";
+import { useColorScheme } from "@/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
