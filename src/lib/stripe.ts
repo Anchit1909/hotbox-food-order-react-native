677ca1faf9e93f8a3710b367a8fddbaefd6e7792
Anchit Sinha <anchit1909@gmail.com>
2024-04-29 17:17:30 +0530

    feat: add stripe data to db

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -19,15 +19,16 @@ const fetchPaymentSheetParams = async (amount: number) => {
 export const initialisePaymentSheet = async (amount: number) => {
   console.log("Initialising payment sheet, for: ", amount);
 
-  const { paymentIntent, publishableKey } = await fetchPaymentSheetParams(
-    amount
-  );
+  const { paymentIntent, publishableKey, customer, ephemeralKey } =
+    await fetchPaymentSheetParams(amount);
 
   if (!paymentIntent || !publishableKey) return;
 
   const result = await initPaymentSheet({
     merchantDisplayName: "anchit",
     paymentIntentClientSecret: paymentIntent,
+    customerId: customer,
+    customerEphemeralKeySecret: ephemeralKey,
     defaultBillingDetails: {
       name: "Jane Doe",
     },
